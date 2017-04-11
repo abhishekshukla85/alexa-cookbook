c5a84abc118ac44d573eeb1bdc1c5ec245f7a60f
Abhishek Shukla <abhishekshukla85@hotmail.com>
2017-04-11 15:54:09 -0700

    AWS IOT and magic response

--- labs/HelloWorld/src/index.js
+++ labs/HelloWorld/src/index.js
@@ -5,13 +5,44 @@ exports.handler = function(event, context, callback) {
 
     // alexa.appId = 'amzn1.echo-sdk-ams.app.1234';
 
-    ///alexa.dynamoDBTableName = 'YourTableName'; // creates new table for session.attributes
+    alexa.dynamoDBTableName = 'HelloWorldTable'; // creates new table for session.attributes
 
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
 var handlers = {
+
+    'NewSession': function() {
+        // any previous session attributes are now loaded from Dynamo into the session attributes
+
+        var todayNow = new Date();
+
+        if(this.attributes['timestamp']) {  // user must have been here before
+            var dateLast = new Date(this.attributes['timestamp']);
+            var timeSpanMS = todayNow.getTime() - dateLast.getTime();
+            var timeSpanHR = Math.floor(timeSpanMS / (1000 * 60 * 60));
+            var timeSpanMIN = Math.floor(timeSpanMS / (1000 * 60 ));
+
+            this.attributes['hoursSinceLast'] = timeSpanHR;
+            this.attributes['minutesSinceLast'] = timeSpanMIN;
+
+            var launchCount = this.attributes['launchCount'];
+            this.attributes['launchCount'] = parseInt(launchCount) + 1;
+
+        } else {  // first use
+            this.attributes['hoursSinceLast'] = 0;
+            this.attributes['minutesSinceLast'] = 0;
+            this.attributes['launchCount'] = 0;
+
+        }
+
+        this.attributes['timestamp'] = todayNow;
+
+        this.emit('MyIntent');
+
+    },
+    
     'LaunchRequest': function () {
         this.emit('MyIntent');
     },
@@ -32,11 +63,19 @@ var handlers = {
     },
 
     'AMAZON.HelpIntent' : function() {
-        this.emit(':ask', 'How can I help you today?');
+        var myName = '';
+        if (this.attributes['name']) {
+            myName = this.attributes['name'];
+        }
+        this.emit(':ask','Hi ' + myName + ', How can I help you today?');
     },
 
     'AMAZON.CancelIntent' : function() {
-        this.emit(':tell', 'I will cancel, this was fun though!');
+        var myName = '';
+        if (this.attributes['name']) {
+            myName = this.attributes['name'];
+        }
+        this.emit(':tell', 'ok ' + myName + '. I will cancel, this was fun though!');
     },
 
     'AMAZON.StopIntent' : function() {
